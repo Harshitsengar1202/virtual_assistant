ca9fcbd087bcd1e61bf140c832513158c5c831a5
Harshitsengar1202 <88681128+Harshitsengar1202@users.noreply.github.com>
2025-04-21 19:05:27 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -83,7 +83,7 @@ def process():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.adjust_for_ambient_noise(source, duration=1)
+        r.adjust_for_ambient_noise(source, duration=0.5)
         audio = r.listen(source)
     try:
         print("Recognizing...")
